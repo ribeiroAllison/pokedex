d0f83d013ba1018041a99f9b1ae988cd6dcd30f5
ribeiroAllison <allisonribeiro@gmail.com>
2023-03-09 14:50:59 -0300

    working Galery .map and async functions

--- src/Components/Galery/Galery.js
+++ src/Components/Galery/Galery.js
@@ -1,26 +1,42 @@
 import { useEffect, useState } from "react"
-import getPokemon from "../../Resources/support"
+import getApiInfo from "../../Resources/support"
+import GaleryPokemon from "../GaleryPokemon/GaleryPokemon";
 
 export default function Galery () {
 
-    const [pokemon, setPokemon] = useState();
+    // const [pokemon, setPokemon] = useState();
+    const [pokemonList, setPokemonList] = useState();
+    // const [id, setId] = useState(1);
+
     
 
     useEffect(() =>{
-        async function pkm(id){
-            let pokeData = await getPokemon(id)
-            setPokemon(pokeData)
-        }
-        pkm(1)
+        async function pkm(){
+            let pokeData = await getApiInfo()
+            // for(let pokemon of pokeData){
+            //     setPokemonList([...pokemonList, pokemon])
+            
+            // }
+            setPokemonList(pokeData.results);
+    }pkm() }, [])
 
-    }, [])
+    
+    
     
     
     
     return(
         <main>
             <div className="galery-card">
-                {/* <h1>{pokemon.name}</h1> */}
+                {
+                    pokemonList.map((pokemon) =>{
+                        return <h1>{pokemon.name}</h1>
+                    }
+                        
+                    
+                        
+                    )
+                }
                 
                 
 
--- src/Resources/support.js
+++ src/Resources/support.js
@@ -1,5 +1,5 @@
 
-async function getApiInfo(id){
+export default async function getApiInfo(){
 
     const baseUrl = 'https://pokeapi.co/api/v2/';
 
@@ -8,7 +8,7 @@ async function getApiInfo(id){
     let results;
     
     try{
-        const response = await fetch(`${baseUrl}${pokemonPath}/${id}`)
+        const response = await fetch(`${baseUrl}${pokemonPath}`)
         
         if(response.ok){
             let jsonResponse = await response.json();
@@ -25,36 +25,35 @@ async function getApiInfo(id){
 
 }
 
-async function getPokemon(id){
+// async function getPokemon(id){
     
-    const pokeObj = await getApiInfo(id);
-    const objId = pokeObj.id;
-    let pokeId = "";
+//     const pokeObj = await getApiInfo(id);
+//     const objId = pokeObj.id;
+//     let pokeId = "";
 
-    if(objId < 10){
-        pokeId = `#000${objId.toString()}`
-    }else if( objId >=10 && objId <100){
-        pokeId = `#00${objId.toString()}`
-    } else if (objId >=100 && objId <1000){
-        pokeId = `#0${objId.toString()}`
-    } else{
-        pokeId = `#${objId.toString()}`
-    }
+//     if(objId < 10){
+//         pokeId = `#000${objId.toString()}`
+//     }else if( objId >=10 && objId <100){
+//         pokeId = `#00${objId.toString()}`
+//     } else if (objId >=100 && objId <1000){
+//         pokeId = `#0${objId.toString()}`
+//     } else{
+//         pokeId = `#${objId.toString()}`
+//     }
     
-    const pokemon = {
-        name: pokeObj.name,
-        height: pokeObj.height,
-        weight: pokeObj.weight,
-        type: pokeObj.types[0].type.name,
-        strId: pokeId,
-        id: pokeObj.id,
-        ability: pokeObj.abilities[0].ability.name,
-        img: pokeObj.sprites.other['official-artwork'].front_default
-    }
+//     const pokemon = {
+//         name: pokeObj.name,
+//         height: pokeObj.height,
+//         weight: pokeObj.weight,
+//         type: pokeObj.types[0].type.name,
+//         strId: pokeId,
+//         id: pokeObj.id,
+//         ability: pokeObj.abilities[0].ability.name,
+//         img: pokeObj.sprites.other['official-artwork'].front_default
+//     }
 
     
-    return pokemon;
-}
+//     return pokemon;
+// }
 
 
-export default getPokemon
